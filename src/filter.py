9163c618ca03e01884f5cca1e2943fe14684d4b2
gusflus <gus.flusser@gmail.com>
2024-11-07 11:13:13 -0800

    filtering

---
+++ src/filter.py
@@ -0,0 +1,19 @@
+from typing import Dict
+
+IGNORED_INCLUDES = ["nullFlavor", "xmlns", "root"]
+
+
+def filter_flattened_json(data: Dict[str, str]) -> Dict[str, str]:
+    """
+    filters out paris if the key includes substring that is in IGNORED_INCLUDES
+    """
+    new_data: Dict[str, str] = {}
+    for key in list(data.keys()):
+        keys = key.split(".")
+        for k in IGNORED_INCLUDES:
+            if k in keys:
+                break
+        else:
+            new_data[key] = data[key]
+
+    return new_data
--- src/flatten.py
+++ src/flatten.py
@@ -7,6 +7,7 @@ def flatten_json(data: Dict[str, Any]) -> Dict[str, Any]:
     e.g. {"a": {"b": 1}} -> {"a.b": 1}
     Handles arrays by including the index in the key
     e.g. {"a": [{"b": 1}, {"c": 2}]} -> {"a.0.b": 1, "a.1.c": 2}
+    also removes ".content" from keys
     """
 
     def flatten_json_helper(data: Any, parent_key: str = "") -> Dict[str, Any]:
@@ -14,13 +15,16 @@ def flatten_json(data: Dict[str, Any]) -> Dict[str, Any]:
         if isinstance(data, dict):
             for key, value in data.items():  # type: ignore
                 new_key = f"{parent_key}.{key}" if parent_key else key  # type: ignore
+                new_key.replace(".content", "")  # type: ignore
                 items.extend(flatten_json_helper(value, new_key).items())  # type: ignore
         elif isinstance(data, list):
             for index, value in enumerate(data):  # type: ignore
                 new_key = f"{parent_key}.{index}" if parent_key else str(index)
+                new_key.replace(".content", "")
                 items.extend(flatten_json_helper(value, new_key).items())
         else:
-            items.append((parent_key, data))
+            items.append((parent_key.replace(".content", ""), data))
+
         return dict(items)
 
     return flatten_json_helper(data)
