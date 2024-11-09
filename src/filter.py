b2da74d00117c96d71f31b86abcabe5670852c7a
gusflus <gus.flusser@gmail.com>
2024-11-08 18:04:47 -0800

    bedrock things

--- src/filter.py
+++ src/filter.py
@@ -1,11 +1,11 @@
 from typing import Dict
 
-IGNORED_INCLUDES = ["nullFlavor", "xmlns", "root"]
+IGNORED_INCLUDES = ["nullFlavor", "xmlns", "root", "width", "border"]
 
 
 def filter_flattened_json(data: Dict[str, str]) -> Dict[str, str]:
     """
-    filters out paris if the key includes substring that is in IGNORED_INCLUDES
+    filters out pairs if the key includes substring that is in IGNORED_INCLUDES
     """
     new_data: Dict[str, str] = {}
     for key in list(data.keys()):
