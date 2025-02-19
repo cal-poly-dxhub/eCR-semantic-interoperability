90c01d36140deb3865926e022160628a27793b27
gusflus <gus.flusser@gmail.com>
2025-02-18 16:41:08 -0800

    next app display file

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,3 +1,14 @@
+import Chunk from "@/components/Chunk";
+import { Paper, Title } from "@mantine/core";
+import jsonObject from "../../public/json_object.json";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Paper m="xl" withBorder p="md" radius="md" mih={200} h="100%">
+      <Title c="blue.5" order={2}>
+        json_object
+      </Title>
+      <Chunk t="test title" j={jsonObject} />
+    </Paper>
+  );
 }
