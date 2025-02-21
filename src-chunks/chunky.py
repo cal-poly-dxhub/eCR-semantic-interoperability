68126440bfcd37cf0bbd402ffb585c2c2bf1ac09
gusflus <gus.flusser@gmail.com>
2025-02-21 14:48:32 -0800

    halfway test.py

--- src-chunks/chunky.py
+++ src-chunks/chunky.py
@@ -229,5 +229,6 @@ def extract_relevant_chunks(
     """
     tree = ET.parse(filename)
     root = tree.getroot()
-    chunks = chunkify_by_hierarchy(root, max_chunk_size)
+    # chunks = chunkify_by_hierarchy(root, max_chunk_size)
+    chunks = chunkify_by_hierarchy_text_tables(root, max_chunk_size, True, True)
     return chunks
